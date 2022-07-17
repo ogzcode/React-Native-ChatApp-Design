8739252cd5644ae10d7537858250e21eb43f53ee
ogzCode <oguzguc44@gmail.com>
2022-07-17 21:17:37 +0300

    Bileşenler oluşturuldu

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
-const textInput = document.querySelector("#text");
-const fontSizeInput = document.querySelector("#font-size");
-const textColorInput = document.querySelector("#text-color");
-
-const widthInput = document.querySelector("#width");
-const heightInput = document.querySelector("#height");
-
-const radiusInput = document.querySelector("#radius");
-const bgColorInput = document.querySelector("#bg-color");
-const borderInput = document.querySelector("#border");
-
-const button = document.querySelector("#button");
-
-const widthOldValue = {old: 0};
-const heightOldValue = {old: 0};
-const radiusOldValue = {old: 0};
-
-function reset(){
-    textInput.value = "";
-    fontSizeInput.value = "";
-    textColorInput.value = "#000";
-
-    widthInput.value = widthInput.min;
-    heightInput.value = heightInput.min;
-
-    radiusInput.value = radiusInput.min;
-    bgColorInput.value = "#000";
-    borderInput.checked = false;
-}
+Vue.component("input-box", {
+    template: `
+    <div class="input__box">
+        <label class="label">{{labelText}}</label>
+        <input 
+            ref="input"
+            :class="className" 
+            :type="inputType" 
+            :placeholder="placeText" 
+            v-model="inputValue" 
+            v-on:input="sendValue"
+            :min="minValue"
+            :max="maxValue"
+            />
+    </div>
+    `,
+    props: {
+        labelText: {
+            type: String,
+            required: true
+        },
+        inputType: {
+            type: String,
+            required: true
+        },
+        placeText: {
+            type: String,
+            required: false
+        },
+        minValue: {
+            type: Number,
+            required: false
+        },
+        maxValue: {
+            type: Number,
+            required: false
+        },
+        className: {
+            type: String,
+            required: false
+        }
+    },
+    data() {
+        return {
+            inputValue: ""
+        };
+    },
+    methods: {
+        sendValue() {
+            this.$emit("send", this.inputValue);
+        },
+        reset() {
+            this.$refs.input.value = 0;
+        }
+    },
+     mounted(){
+        if (this.inputType == "range") {
+            this.reset();
+        }
+     }
+});
 
 function changeStyleWithRange(nodeValue, realNodeValue, oldValue){
     let result = 0;
@@ -44,65 +76,65 @@ function changeStyleWithRange(nodeValue, realNodeValue, oldValue){
     return result;
 }
 
+const widthOldValue = {old: 0};
+const heightOldValue = {old: 0};
+const radiusOldValue = {old: 0};
 
-
-textInput.addEventListener("input", () => {
-    button.innerText = textInput.value;
-    if (textInput.value === ""){
-        button.innerText = "Text";
+new Vue({
+    el: "#app",
+    data: {
+        buttonText: "",
+        styles: {
+            fontSize: "",
+            color: "",
+            backgroundColor: ""
+        }
+    },
+    methods: {
+        getText(value) {
+            this.buttonText = value;
+        },
+        getFontSize(value) {
+            if (parseInt(value) < 12) {
+                this.styles.fontSize = "12px";
+            }
+            else if (parseInt(value) > 64) {
+                this.styles.fontSize = "64px";
+            }
+            else {
+                this.styles.fontSize = value + "px";
+            }
+        },
+        getTextColor(value) {
+            this.styles.color = value;
+        },
+        getWidth(value) {
+            this.$refs.btn.style.width = changeStyleWithRange(parseInt(value), 
+            parseInt(window.getComputedStyle(this.$refs.btn).width), widthOldValue) + "px";
+        },
+        getHeight(value){
+            this.$refs.btn.style.height = changeStyleWithRange(parseInt(value), 
+            parseInt(window.getComputedStyle(this.$refs.btn).height), heightOldValue) + "px"
+        },
+        getRadius(value){
+            this.$refs.btn.style.borderRadius = changeStyleWithRange(parseInt(value), 
+            parseInt(window.getComputedStyle(this.$refs.btn).borderRadius), radiusOldValue) + "px"
+        },
+        getBackgroundColor(value) {
+            this.styles.backgroundColor = value;
+        },
+        getBorder(value){
+            if (!value){
+                this.$refs.btn.style.border = "1px solid gray";
+            }
+            else {
+                this.$refs.btn.style.border = "none";
+            }
+        }
+    },
+    computed: {
+        printText() {
+            return this.buttonText == "" ? "Text" : this.buttonText;
+        }
     }
 });
-
-fontSizeInput.addEventListener("input", () => {
-    button.style.fontSize = `${fontSizeInput.value}px`;
-    if (fontSizeInput.value < 10){
-        button.style.fontSize = `10px`;
-    }
-    else if (fontSizeInput.value > 64){
-        button.style.fontSize = `64px`
-    }
-});
-
-textColorInput.addEventListener("change", () => {
-    button.style.color = textColorInput.value;
-});
-
-
-
-
-widthInput.addEventListener("input" , () => {
-    let result = changeStyleWithRange(parseInt(widthInput.value), parseInt(window.getComputedStyle(button).width),
-    widthOldValue);
-    button.style.width = `${result}px`;
-});
-
-heightInput.addEventListener("input", () => {
-    let result = changeStyleWithRange(parseInt(heightInput.value), parseInt(window.getComputedStyle(button).height),
-    heightOldValue);
-    button.style.height = `${result}px`;
-});
-
-
-
-
-radiusInput.addEventListener("input", () => {
-    let result = changeStyleWithRange(parseInt(radiusInput.value), parseInt(window.getComputedStyle(button).borderRadius),
-    radiusOldValue);
-    button.style.borderRadius = `${result}px`;
-});
-
-bgColorInput.addEventListener("change", () => {
-    button.style.backgroundColor = bgColorInput.value;
-});
-
-borderInput.addEventListener("change", () => {
-    if (borderInput.checked == true){
-        button.style.border = "1px solid gray";
-    }
-    else {
-        button.style.border = "none";
-    }
-});
-
-reset();
-
