fe61fdad416352645ca843ec1f9b352c268475dd
ogzCode <oguzguc44@gmail.com>
2025-01-22 10:37:21 +0300

    Add OtpControl screen to StackNavigator for enhanced authentication flow

--- src/navigation/StackNavigator.tsx
+++ src/navigation/StackNavigator.tsx
@@ -1,13 +1,14 @@
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from '../screen/auth/Login';
-
+import OtpControl from '../screen/auth/OtpControl';
 const Stack = createStackNavigator();
 
 export default function StackNavigator() {
   return (
     <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="OtpControl" component={OtpControl} />
     </Stack.Navigator>
   );
 }
